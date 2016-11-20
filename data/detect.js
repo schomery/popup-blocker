ea3ef32b308329190883caa550a5d019f1ecb710
Jeremy Schomery <jeremy.schomery@yahoo.com>
2016-11-21 00:08:07 +0330

    improving communication channel

--- data/detect.js
+++ data/detect.js
@@ -1,3 +1,4 @@
+/* globals cloneInto */
 'use strict';
 
 // Firefox does not allow to define non-configurable property over the "window" object.
@@ -5,42 +6,40 @@ var configurable = navigator.userAgent.indexOf('Firefox') !== -1;
 
 var requests = {};
 var commands = {};
+var active = true;
 
-// Generating 40 onetime random numbers
-var oneTimeKeys = [];
-for (let i = 0; i < 40; i++) {
-  oneTimeKeys.push(Math.random());
+window.cloneInto = typeof cloneInto  === 'undefined' ? function (a) {
+  return a;
+} : cloneInto;
+
+function post (name, value) {
+  document.dispatchEvent(new CustomEvent(name, {
+    detail: cloneInto(value, document.defaultView),
+    bubbles: false,
+    cancelable: false
+  }));
 }
 
-window.addEventListener('message', e => {
-  let request = e.data;
-  if (request.cmd === 'popup-request') {
-    chrome.runtime.sendMessage(e.data);
-    requests[request.id] = e.data;
-    commands[request.id] = commands[request.id] || [];
-  }
-  else if (request.cmd === 'popup-request-additional') {
-    commands[request.id] = commands[request.id] || [];
-    commands[request.id].push(e.data);
-  }
+document.addEventListener('ppp-blocker-create', e => {
+  let request = e.detail;
+  chrome.runtime.sendMessage(request);
+  requests[request.id] = e.detail;
+  commands[request.id] = commands[request.id] || [];
+});
+document.addEventListener('ppp-blocker-append', e => {
+  let request = e.detail;
+  commands[request.id] = commands[request.id] || [];
+  commands[request.id].push(request);
 });
 
-chrome.runtime.onMessage.addListener((request) => {
+chrome.runtime.onMessage.addListener(request => {
   let id = request.id;
 
   // apply popup accept on the context where it is originally requested
   if (request.cmd === 'popup-accepted' && requests[id]) {
-    let win = window.open.apply(window, requests[id].arguments);
-    commands[id].forEach(obj => {
-      try {
-        if (obj.name === 'focus') {
-          win.focus();
-        }
-        else {
-          win.document[obj.name].apply(win.document, obj.arguments);
-        }
-      }
-      catch (e) {}
+    post('ppp-blocker-exe', {
+      arguments: requests[id].arguments,
+      commands: commands[id]
     });
   }
   // clean up
@@ -57,7 +56,19 @@ chrome.runtime.onMessage.addListener((request) => {
 
 var script = document.createElement('script');
 script.textContent = `
-(function (wPointer, dPointer, preventDefault, stopPropagation, stopImmediatePropagation, write, isEnabled, isDomain, whitelist, keys, activeElement) {
+(function (ePointer, wPointer, dPointer, preventDefault, stopPropagation, stopImmediatePropagation, write, isEnabled, isDomain, whitelist, activeElement) {
+  // protection channel; do not allow custom event generation with names starting with "ppp-blocker"
+  Object.defineProperty(window, 'CustomEvent', {
+    writable: false,
+    configurable: ${configurable},
+    value: function (name, prps) {
+      if (name && !name.startsWith('ppp-blocker')) {
+        return new ePointer(name, prps);
+      }
+      return new ePointer('blocked-request');
+    }
+  });
+  // is this URL valid
   function permit (url) {
     // white-list section
     try {
@@ -83,6 +94,13 @@ script.textContent = `
     return false;
   }
 
+  function post (name, value, top) {
+    window[top ? 'top' : 'self'].document.dispatchEvent(new ePointer(name, {
+      detail: value,
+      bubbles: false,
+      cancelable: false
+    }));
+  }
   Object.defineProperty(window, 'open', {
     writable: false,
     configurable: ${configurable},
@@ -98,57 +116,52 @@ script.textContent = `
             selected.dataset.popupblocker = selected.dataset.popupblocker || id;
           }
           //
-          window.postMessage({
+          post('ppp-blocker-create', {
             cmd: 'popup-request',
             type: 'window.open',
             url,
             id,
             arguments: Array.from(arguments),
             tag: selected.dataset.popupblocker
-          }, '*');
+          });
         }, 100);
 
         return {
           document: {
             open: function () {
-              window.postMessage({
-                cmd: 'popup-request-additional',
+              post('ppp-blocker-append', {
                 name: 'open',
                 arguments: Array.from(arguments),
                 id
-              }, '*');
+              });
             },
             write: function () {
-              window.postMessage({
-                cmd: 'popup-request-additional',
+              post('ppp-blocker-append', {
                 name: 'write',
                 arguments: Array.from(arguments),
                 id
-              }, '*');
+              });
             },
             close: function () {
-              window.postMessage({
-                cmd: 'popup-request-additional',
+              post('ppp-blocker-append', {
                 name: 'close',
                 arguments: Array.from(arguments),
                 id
-              }, '*');
+              });
             }
           },
           focus: function () {
-            window.postMessage({
-              cmd: 'popup-request-additional',
+            post('ppp-blocker-append', {
               name: 'focus',
               id
-            }, '*');
+            });
           },
           close: function () {
-            window.postMessage({
-              cmd: 'popup-request-additional',
+            post('ppp-blocker-append', {
               name: 'close',
               arguments: Array.from(arguments),
               id
-            }, '*');
+            });
           }
         }
       }
@@ -170,26 +183,19 @@ script.textContent = `
       if ((a.target.toLowerCase() === '_blank' || a.target.toLowerCase() === '_parent' || base || dynamic) && e.button === 0 && !e.metaKey) {
         if (!permit(a.href)) {
           let id = Math.random();
-          window[child ? 'top' : 'self'].postMessage({
-            cmd: 'popup-request',
-            type: 'target._blank',
-            url: a.href,
-            arguments: [a.href],
-            id
-          }, '*');
+          post('ppp-blocker-create', {
+              cmd: 'popup-request',
+              type: 'target._blank',
+              url: a.href,
+              arguments: [a.href],
+              id
+          }, child);
 
           e.preventDefault();
         }
       }
     }
   };
-  /*
-  document.addEventListener('DOMContentLoaded', () => {
-    [...document.querySelectorAll('a')].forEach(a => {
-      a.addEventListener('click', onclick);
-    });
-  });
-  */
   document.addEventListener('click', onclick);
 
   // dynamic "a" elements
@@ -245,32 +251,31 @@ script.textContent = `
     }
   });
 
-  window.addEventListener('message', e => {
-    if (e.data.cmd === 'change-status' || e.data.cmd === 'configure') {
-      let key = e.data.key;
-      let index = keys.indexOf(key);
-      if (index !== -1) {
-        // if key is valid remove it from the list of valid keys
-        keys.splice(index, 1);
-      }
-      else {
-        return;
-      }
-    }
-    if (e.data.cmd === 'change-status') {
-        isEnabled = e.data.value;
-    }
-    else if (e.data.cmd === 'configure') {
-      isEnabled = e.data.enabled;
-      isDomain = e.data.domain;
-      whitelist = e.data.whitelist;
-      if (!e.data.target) {
-        window.removeEventListener('click', onclick);
-      }
+  // configurations
+  document.addEventListener('ppp-blocker-status', e => isEnabled = e.detail.value);
+  document.addEventListener('ppp-blocker-configure', e => {
+    isEnabled = e.detail.enabled;
+    isDomain = e.detail.domain;
+    whitelist = e.detail.whitelist;
+    if (!e.detail.target) {
+      document.removeEventListener('click', onclick);
     }
   });
-
+  // execute
+  document.addEventListener('ppp-blocker-exe', e => {
+    let request = e.detail;
+    let win = wPointer.apply(window, request.arguments);
+    request.commands.forEach(obj => {
+      if (obj.name === 'focus') {
+        win.focus();
+      }
+      else {
+        win.document[obj.name].apply(win.document, obj.arguments);
+      }
+    });
+  });
 })(
+  window.CustomEvent,
   window.open,
   document.createElement,
   MouseEvent.prototype.preventDefault,
@@ -279,57 +284,37 @@ script.textContent = `
   document.write,
   true,
   false,
-  [],
-  [${oneTimeKeys}]
+  []
 );`;
 document.documentElement.appendChild(script);
 document.documentElement.removeChild(script);
 
-/* Enabling and disabling the popup-blocker
- * The extension uses insecure window.postMessage to enable/disable the popup blocker;
- * To protect against malicious scripts from disabling the blocker, we are passing onetime keys along with the command
-*/
-var active = true;
 chrome.storage.local.get({
   'enabled': true,
   'target': true,
   'domain': false,
   'popup-hosts': ['google.com', 'bing.com']
 }, prefs => {
-  window.postMessage({
+  post('ppp-blocker-configure', {
     cmd: 'configure',
     enabled: prefs.enabled,
     target: prefs.target,
     domain: prefs.domain,
-    whitelist: prefs['popup-hosts'],
-    key: oneTimeKeys.shift()
-  }, '*');
+    whitelist: prefs['popup-hosts']
+  });
 });
+
 chrome.storage.onChanged.addListener(obj => {
   if (obj.enabled && active) {
-    if (oneTimeKeys.length) {
-      window.postMessage({
-        cmd: 'change-status',
-        value: obj.enabled.newValue,
-        key: oneTimeKeys.shift()
-      }, '*');
-    }
-    // only display warning once
-    else if (window === window.top) {
-      window.alert('Stack limit\n\nCannot change the popup-blocker status anymore. Please refresh this page.');
-    }
+    post('ppp-blocker-status', {value: obj.enabled.newValue});
   }
 });
-// is top domain white-listed
+// is top domain white-listed.
 chrome.runtime.sendMessage({
   cmd: 'validate'
 }, (response) => {
-  if (response.valid) {
+  if (response && response.valid) {
     active = false;
-    window.postMessage({
-      cmd: 'change-status',
-      value: false,
-      key: oneTimeKeys.shift()
-    }, '*');
+    post('ppp-blocker-status', {vslue: false});
   }
 });
