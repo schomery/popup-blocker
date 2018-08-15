5a85a1f842e6afe392bf541d2dd229e37cb28602
Jeremy Schomery <jeremy.schomery@yahoo.com>
2018-08-15 12:52:41 +0430

    make sure the notification box is accessible

--- src/data/iframe.js
+++ src/data/iframe.js
@@ -1,38 +1,48 @@
 'use strict';
 
-var iframe;
-
-chrome.runtime.onMessage.addListener(request => {
-  if (request.cmd === 'popup-request' && !iframe) {
-    iframe = document.createElement('iframe');
-    iframe.src = chrome.runtime.getURL('data/ui/ui.html?request=' + encodeURIComponent(JSON.stringify(request)));
-    iframe.setAttribute('style', `
-      z-index: 2147483649;
-      position: fixed;
-      right: 10px;
-      top: 10px;
-      width: 350px;
-      max-width: 80vw;
-      height: 85px;
-      border: none;
-      background: transparent;
-      border-radius: 0;
-    `);
-    document.body.appendChild(iframe);
-  }
-});
-
-window.addEventListener('message', e => {
-  const request = e.data;
-  if (request && request.method === 'ppp-resize') {
-    e.preventDefault();
-    e.stopPropagation();
-    if (request.hide) {
-      iframe.remove();
-      iframe = null;
+{
+  let iframe;
+  chrome.runtime.onMessage.addListener(request => {
+    if (request.cmd === 'popup-request') {
+      if (!iframe) {
+        iframe = document.createElement('iframe');
+        iframe.src = chrome.runtime.getURL('data/ui/ui.html?request=' + encodeURIComponent(JSON.stringify(request)));
+        iframe.setAttribute('style', `
+          z-index: 2147483649;
+          position: fixed;
+          right: 10px;
+          top: 10px;
+          width: 350px;
+          max-width: 80vw;
+          height: 85px;
+          border: none;
+          background: transparent;
+          border-radius: 0;
+        `);
+        document.body.appendChild(iframe);
+      }
+      // always reattach to make sure the iframe is accessible
+      const es = document.elementsFromPoint(iframe.offsetLeft, iframe.offsetTop);
+      const index = es.indexOf(iframe);
+      if (index !== 0) {
+        // this is going to refresh the iframe
+        document.body.appendChild(iframe);
+      }
     }
-    else {
-      iframe.style.height = request.height + 'px';
+  });
+  window.addEventListener('message', e => {
+    const request = e.data;
+    if (request && request.method === 'ppp-resize') {
+      e.preventDefault();
+      e.stopPropagation();
+      if (request.hide) {
+        iframe.remove();
+        iframe = null;
+      }
+      else {
+        iframe.style.height = request.height + 'px';
+      }
     }
-  }
-});
+  });
+}
+
